25381fab362865c63319cbe1d1663bdfdaa6caf8
Erick Zacarias <Erick Zacarias>
2023-05-01 20:05:45 -0600

    funcionalidades (encriptar, desencriptar y copiar) de la pagina funcionando pendiente de estilo solamente

--- js/Funcionalidades.js
+++ js/Funcionalidades.js
@@ -1,20 +1,58 @@
 var vocales = ["e", "i", "a", "o", "u"];
 var encriptado = ["enter", "imes", "ai", "ober", "ufat"];
 
-export function encriptar(cadena) {
+//************FUNCIONES EXPORTADAS********************************
+
+export function encriptar() {
+  var cadena = capturarTexto();
+
+  validar(cadena);
 
   for (let i = 0; i < cadena.length; i++) {
     var cadena2 = cadena.replaceAll(vocales[i], encriptado[i]);
     cadena = cadena2;
   }
-  return cadena;
+  escribirTexto(cadena);
 }
 
-export function desencriptar (cadena){
+export function desencriptar(cadena) {
+  var cadena = capturarTexto();
+  validar(cadena);
+
   for (let i = 0; i < cadena.length; i++) {
     var cadena2 = cadena.replaceAll(encriptado[i], vocales[i]);
     cadena = cadena2;
   }
-  return cadena;
+  escribirTexto(cadena);
 }
 
+export function copiar() {
+  var cEncriptada = document.getElementById("area-resultado");
+  var boton = document.getElementById("boton-copiar");
+
+  navigator.clipboard.writeText(cEncriptada.textContent);
+
+  boton.textContent = "Texto Copiado";
+
+  setTimeout(() => {
+    boton.textContent = "copiar";
+  }, 2000);
+}
+
+//********************************FUNCIONES DE AYUDA**********************/
+
+function validar(cadena) {
+  if (!cadena) {
+    alert("Por favor, ingrese un texto.");
+  }
+  return;
+}
+
+function escribirTexto(cadena) {
+  document.getElementById("area-resultado").textContent = cadena;
+}
+
+function capturarTexto() {
+  let cadena = document.getElementById("area-encriptado").value;
+  return cadena;
+}
--- js/codigo.js
+++ js/codigo.js
@@ -1,6 +1,11 @@
 import * as funcion from "./Funcionalidades.js";
 
-var cadena = funcion.encriptar("erick");
-var cadena = funcion.desencriptar("enterrimesck");
+var e = document.getElementById("boton-encriptar");
+var de = document.getElementById("boton-desencriptar");
+var c = document.getElementById("boton-copiar");
 
-console.log(cadena);
+e.addEventListener("click", funcion.encriptar);
+
+de.addEventListener("click", funcion.desencriptar);
+
+c.addEventListener("click", funcion.copiar);
