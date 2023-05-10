db1c6081b86776747256e4d8b09c9d5191b4412a
Erick Zacarias <Erick Zacarias>
2023-05-10 17:03:20 -0600

    funcionamiento y vista web funcionando, pendiente responsive y modo nocturno

--- js/Funcionalidades.js
+++ js/Funcionalidades.js
@@ -30,13 +30,25 @@ export function copiar() {
   var cEncriptada = document.getElementById("area-resultado");
   var boton = document.getElementById("boton-copiar");
 
-  navigator.clipboard.writeText(cEncriptada.textContent);
+  navigator.clipboard.writeText(cEncriptada.value);
 
-  boton.textContent = "Texto Copiado";
+  boton.textContent = "¡Texto Copiado!";
+
+  cEncriptada.value = "";
 
   setTimeout(() => {
-    boton.textContent = "copiar";
+    boton.textContent = "¡Copiar!";
   }, 2000);
+}
+
+export function vista() {
+  var ae = document.getElementById("area-encriptado");
+
+  if (ae.value.length != 0) {
+    vistaInput();
+  } else {
+    vistaDefault();
+  }
 }
 
 //********************************FUNCIONES DE AYUDA**********************/
@@ -49,10 +61,32 @@ function validar(cadena) {
 }
 
 function escribirTexto(cadena) {
-  document.getElementById("area-resultado").textContent = cadena;
+  document.getElementById("area-resultado").value = cadena;
 }
 
 function capturarTexto() {
   let cadena = document.getElementById("area-encriptado").value;
   return cadena;
+}
+
+function vistaDefault() {
+  document.getElementById("txt-ini").style.display = "block";
+  document.getElementById("img-aside").style.display = "block";
+  document.getElementById("texto-img-resultado").style.display = "block";
+  document.getElementById("texto-img-blanco").style.display = "block";
+  document.getElementById("boton-copiar").style.display = "none";
+  document.getElementById("area-resultado").style.display = "none";
+
+  var s2 = document.getElementById("area-resultado");
+
+  s2.value = "";
+}
+
+function vistaInput() {
+  document.getElementById("txt-ini").style.display = "none";
+  document.getElementById("img-aside").style.display = "none";
+  document.getElementById("texto-img-resultado").style.display = "none";
+  document.getElementById("texto-img-blanco").style.display = "none";
+  document.getElementById("boton-copiar").style.display = "block";
+  document.getElementById("area-resultado").style.display = "block";
 }
--- js/codigo.js
+++ js/codigo.js
@@ -3,9 +3,12 @@ import * as funcion from "./Funcionalidades.js";
 var e = document.getElementById("boton-encriptar");
 var de = document.getElementById("boton-desencriptar");
 var c = document.getElementById("boton-copiar");
+var ae = document.getElementById("area-encriptado");
 
 e.addEventListener("click", funcion.encriptar);
 
 de.addEventListener("click", funcion.desencriptar);
 
 c.addEventListener("click", funcion.copiar);
+
+ae.addEventListener("input", funcion.vista);
